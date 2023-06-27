f13f35600654f3c4477cd85c2bab5247233b0645
Leopelo <ludenaleonela@gmail.com>
2023-06-26 23:33:04 -0400

    set models and filled seed files.

--- models/Project.js
+++
@@ -1,4 +0,0 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-
-
--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+
 class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
