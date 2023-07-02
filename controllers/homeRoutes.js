40ea461874b6545637fd876cf42b343ba64e99a6
Baba II <127648521+allister-seras@users.noreply.github.com>
2023-07-01 23:27:12 -0700

    set up handlebars main, homepage and log in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,36 @@ const router = require('express').Router();
 const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    // Pass serialized data and session flag into template
+    res.render('homepage');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+router.get('/project/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const project = projectData.get({ plain: true });
+
+    res.render('project', {
+      ...project,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
