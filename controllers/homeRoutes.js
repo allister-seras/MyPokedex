a2f39539d304279b7bbd7b63199755ad7e16b02b
Baba II <127648521+allister-seras@users.noreply.github.com>
2023-07-26 21:45:34 -0700

    added styling for profile and pokemon pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -54,7 +54,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Team }],
+      include: [{ model: Team, Pokemon }],
     });
 
     const user = userData.get({ plain: true });
